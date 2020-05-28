26fffac5e42b200b382680b52225e64fe6099474
susan hofmann <susanhofmannnyc@icloud.com>
2020-05-28 19:56:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson";
-greeting = "Hello, ";
 
-var greetSpecialGuest = greeting + specialGuest + "!";
+var greetSpecialGuest = "Hello " + specialGuest + "!";
 
 var topic = "space";
 
